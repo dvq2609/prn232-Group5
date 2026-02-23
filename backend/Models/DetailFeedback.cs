4492c0e4259222e24600f6eb019888e73c315e7f
dvquyen2609 <chinieko0@gmail.com>
2026-02-23 13:51:06 +0700

    first commit

---
+++ backend/Models/DetailFeedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class DetailFeedback
+{
+    public int Id { get; set; }
+
+    public int? DeliveryOnTime { get; set; }
+
+    public int? ExactSame { get; set; }
+
+    public int? Communication { get; set; }
+
+    public int FeedbackId { get; set; }
+}
